2d128b9d5672097a535b274b25c52493ff9c7a5e
Markus Maasikas <markus@hotmail.com>
2019-12-12 14:35:15 -0500

    updated css

---
+++ js/index.js
@@ -0,0 +1,176 @@
+/* navbar modify */
+
+
+$(document).ready(function() {
+  //  using strict mode It helps you to write cleaner code, like preventing you from using undeclared variables.
+  'use strict';
+
+  $(window).scroll(function() {
+
+    'use strict';
+
+    if($(window).scrollTop() < 80) {
+
+      $('.navbar').css ({
+        'margin-top': '-100px',
+        'opacity': '0'
+      });
+
+      $('.navbar-default').css({
+        'background-color': 'rgba(59, 59, 0)'
+      });
+
+     } else {
+        $('.navbar').css ({
+          'margin-top': '0px',
+          'opacity': '1'
+        });
+
+        $('.navbar-default').css({
+         'background-color': 'rgba(59, 59, 0)',
+         'border-color': '#444'
+
+        });
+
+        $('.navbar-brand img').css({
+          'height': '35px',
+          'padding-top': '0px'
+
+        });
+
+        $('.navbar-nav > li > a').css({
+          'padding-top': '15px'
+        });
+
+        }
+
+  });
+
+
+ });
+
+//adding smooth scrolling
+
+$(document).ready(function() {
+
+  'use strict';
+
+  $('nav-item,[href="#top"]')
+  .click(function(event) {
+
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        // event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+
+
+        // function() {
+        //   // Callback after animation
+        //   // Must change focus!
+        //   var $target = $(target);
+        //   $target.focus();
+        //   if ($target.is(":focus")) { // Checking if the target was focused
+        //     return false;
+        //   } else {
+        //     $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+        //     $target.focus(); // Set focus again
+        //   };
+        // });
+       }
+     }
+   });
+
+});
+
+/*working on making the menu active on click */
+
+$(document).ready(function() {
+  'use strict';
+
+  $('.navbar-nav li a').click(function() {
+    'use strict';
+
+    $('.navbar-nav li a').parent().removeClass("active");
+
+    $(this).parent().addClass("active");
+  });
+});
+
+
+// / highlights each section on the menu when scrolling/
+
+$(document).ready(function() {
+  'use strict';
+
+  $(window).scroll(function() {
+
+    'use strict';
+
+    $("section").each(function() {
+         'use strict';
+          var bb = $(this).attr("id");
+          var hei = $(this).outerHeight();
+          var grttop =$(this).offset().top - 70;
+
+          if ($(window).scrollTop() > grttop && $(window).scrollTop() < grttop + hei) {
+
+            $(".navbar-nav li a[href='#" + bb + "']").parent().addClass("active");
+
+
+          }else {
+            $(".navbar-nav li a[href='#" + bb + "']").parent().removeClass("active");
+          }
+
+   });
+   });
+ });
+
+
+// adding some padding to header
+
+// $(document).ready(function() {
+
+//  'use strict';
+
+//  setInterval(function() {
+
+//    'use strict';
+//    var windowHeight = $(window).height();
+//    var containerHeight = $(".header-container").height();
+//    var padTop = windowHeight - containerHeight;
+//    $(".header-container").css({
+
+//      'padding-top': Math.round( padTop / 2) + 'px',
+//      'padding-bottom': Math.round( padTop / 2) + 'px',
+//    ));
+
+//  }, 10)
+
+// });
+
+//adding sliders to screen
+// $(document).ready(funciton(){
+
+//       'use strict';
+//       $('.slider').bxSlider({
+
+//         sliderwidth: 292.5,
+//         auto:true,
+//         minSlides: 1,
+//         maxSlides: 3,
+//         slideMargin: 50
+//       });
+
+// });
