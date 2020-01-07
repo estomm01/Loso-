69f4bec4df3a44aa44b3ee41cff051c12eafb703
Markus Maasikas <markus@hotmail.com>
2020-01-06 21:28:25 -0500

    updated counters

--- js/index.js
+++ js/index.js
@@ -138,6 +138,22 @@ $(document).ready(function() {
  });
 
 
+$(document).ready(function() {
+$('.counter-num').counterUp({
+    delay: 10,
+    time: 2000
+});
+});
+
+// updating counter seciotn
+
+// $(document).ready(function() {
+//   $('.counter-num').counterUp({
+//     delay: 10,
+//     time: 2000
+// });
+// };
+
 // adding some padding to header
 
 // $(document).ready(function() {
